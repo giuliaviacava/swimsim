ed18b7611ffd8a18195f09bbdca322c99e8f7688
davinahuang07 <davinahuang07@gmail.com>
2026-04-19 20:58:34 +0000

    Sun, Apr 19, 2026, 4:58 PM -04:00

---
+++ configuration.py
@@ -0,0 +1,19 @@
+EVENTS = [
+    "50 Free",
+    "100 Free",
+    "200 Free",
+    "500 Free",
+    "1000 Free",
+    "100 Back",
+    "200 Back",
+    "100 Breast",
+    "200 Breast",
+    "100 Fly",
+    "200 Fly",
+    "200 IM",
+    "400 IM",
+]
+
+SCORING = [9, 4, 3, 2, 1]
+MAX_EVENTS_PER_SWIMMER = 3
+RACE_VARIATION = 0.02
