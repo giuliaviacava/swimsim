901f504cdb7f5424cc2d00c26ac4686b8d3b35c2
davinahuang07 <davinahuang07@gmail.com>
2026-04-19 21:01:13 +0000

    Sun, Apr 19, 2026, 5:01 PM -04:00

--- configuration.py
+++ configuration.py
@@ -15,5 +15,10 @@ EVENTS = [
 ]
 
 SCORING = [9, 4, 3, 2, 1]
+
 MAX_EVENTS_PER_SWIMMER = 3
-RACE_VARIATION = 0.02
+ENTRIES_PER_TEAM_PER_EVENT = 3
+
+# Race-day variation, swimmers can go up to 2% faster or 5% slower than their best time
+MIN_VARIATION = -0.02
+MAX_VARIATION = 0.05
