bf55f6cd0e628da8c54b2d961eb76f31d8fafa98
davinahuang07 <davinahuang07@gmail.com>
2026-04-20 02:38:40 +0000

    Sun, Apr 19, 2026, 10:38 PM -04:00

--- team.py
+++ team.py
@@ -10,12 +10,9 @@ class Team:
     def add_points(self, points):
         self.score += points
 
-    def reset_score(self):
-        self.score = 0
-
     def clear_all_assignments(self):
         for swimmer in self.swimmers:
             swimmer.clear_assignments()
 
-    def __repr__(self):
+    def __str__(self):
         return f"Team({self.name}, score={self.score})"
