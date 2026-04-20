4434801b11504f56f1a2a836b4371d1896cf44dc
davinahuang07 <davinahuang07@gmail.com>
2026-04-20 01:38:18 +0000

    Sun, Apr 19, 2026, 9:38 PM -04:00

--- main.py
+++ main.py
@@ -1,12 +1,41 @@
-from configuration import EVENTS
-from dataloader import load_teams_from_csv
+import csv
+
+from swim import Swimmer
+from team import Team
 from meet import Meet
+from configuration import EVENTS
+
+
+def load_teams_from_txt(filename):
+    teams = {}
+
+    with open(filename, newline="", encoding="utf-8") as file:
+        reader = csv.DictReader(file)
+
+        for row in reader:
+            name = row["name"].strip()
+            team_name = row["team"].strip()
+
+            best_times = {}
+            for event in EVENTS:
+                value = row[event].strip()
+                if value != "NA":
+                    best_times[event] = float(value)
+
+            swimmer = Swimmer(name, team_name, best_times)
+
+            if team_name not in teams:
+                teams[team_name] = Team(team_name)
+
+            teams[team_name].add_swimmer(swimmer)
+
+    return teams
 
 
 def main():
     filename = "rosters.txt"
 
-    all_teams = load_teams_from_csv(filename)
+    all_teams = load_teams_from_txt(filename)
 
     selected_team_names = ["Harvard", "Princeton"]
     teams = {}
