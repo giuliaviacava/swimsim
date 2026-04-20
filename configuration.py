7bcc45c7396856c9b519f75459a3d3a66e05d4fd
davinahuang07 <davinahuang07@gmail.com>
2026-04-20 02:14:36 +0000

    Sun, Apr 19, 2026, 10:14 PM -04:00

--- configuration.py
+++ configuration.py
@@ -3,22 +3,7 @@
 Defines what events exist, how scoring works, participation limits, random add/drop time variations
 '''
 
-EVENTS = [
-    "50 Free",
-    "100 Free",
-    "200 Free",
-    "500 Free",
-    "1000 Free",
-    "100 Back",
-    "200 Back",
-    "100 Breast",
-    "200 Breast",
-    "100 Fly",
-    "200 Fly",
-    "200 IM",
-    "400 IM",
-]
-
+EVENTS = ["50 Free", "100 Free", "200 Free", "500 Free", "1000 Free", "100 Back", "200 Back", "100 Breast", "200 Breast", "100 Fly", "200 Fly", "200 IM", "400 IM",]
 SCORING = [9, 4, 3, 2, 1]
 
 MAX_EVENTS_PER_SWIMMER = 3
