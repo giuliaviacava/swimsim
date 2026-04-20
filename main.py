923081d58b4da159d7f6dbb6d4bd88ae02e52956
davinahuang07 <davinahuang07@gmail.com>
2026-04-20 01:57:54 +0000

    Sun, Apr 19, 2026, 9:57 PM -04:00

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from configuration import EVENTS
 def get_teams(filename):
     teams = {}
 
-    with open(f"{filename}", "r", encoding="utf-8") as file:
+    with open(filename, "r", encoding="utf-8") as file:
         header = file.readline().strip().split(",")
 
         while True:
@@ -23,7 +23,7 @@ def get_teams(filename):
 
             row = dict(zip(header, parts))
 
-            name = row["Name"].strip()
+            name = row["name"].strip()
             team_name = row["team"].strip()
 
             best_times = {}
@@ -50,16 +50,16 @@ def main():
     while True:
         school1 = input("Choose School #1: ").strip()
         if school1 not in ivy_schools:
-            print("Sorry, but this is an Ivy League dual meet simulator. Please choose an Ivy League school.\n")
+            print("Sorry, but this is an Ivy League dual meet simulator. Please choose an Ivy League school.")
             continue
         break
     while True:
         school2 = input("Choose School #2: ").strip()
         if school2 == school1:
-            print("You already chose this school for School #1, please choose a different school for School #2.\n")
+            print("You already chose this school for School #1, please choose a different school for School #2.")
             continue
         if school2 not in ivy_schools:
-            print("Sorry, but this is an Ivy League dual meet simulator. Please choose an Ivy League school.\n")
+            print("Sorry, but this is an Ivy League dual meet simulator. Please choose an Ivy League school.")
             continue
         break
     teams = {school1: all_teams[school1], school2: all_teams[school2],}
