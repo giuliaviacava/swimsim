f73e1f786442fe7f1510d20986ffd51286c57c61
davinahuang07 <davinahuang07@gmail.com>
2026-04-20 02:28:10 +0000

    Sun, Apr 19, 2026, 10:28 PM -04:00

--- main.py
+++ main.py
@@ -5,24 +5,30 @@ from configuration import EVENTS
 
 def get_teams(filename):
     teams = {}
-    # 
     with open(filename, "r", encoding="utf-8") as file:
+        # Reads 1st line, creates a list of phrases called header that were separated by commas in the original line (should be name, team, event #1, event #2, ...)
         header = file.readline().strip().split(",")
+
         while True:
+            # Reads a line, creates a list of phrases called parts that were separated by commas in the original line
             line = file.readline()
             if line == "":
                 break
             parts = line.split(",")
 
+            # Pairs the 2 lists into a dictionary, creates list of names and list of team names
             row = dict(zip(header, parts))
             name = row["name"]
             team_name = row["team"]
 
+            # Loops through all events, if best time exists then assign best time to event
             best_times = {}
             for event in EVENTS:
-                value = row[event].strip()
-                if value != "NA":
-                    best_times[event] = float(value)
+                best_time = row[event].strip()
+                if best_time != "NA":
+                    best_times[event] = float(best_time)
+
+            # Creates Swimmer object, creates Team object if team name is not in list of teams, add swimmer to team
             swimmer = Swimmer(name, team_name, best_times)
             if team_name not in teams:
                 teams[team_name] = Team(team_name)
@@ -33,6 +39,8 @@ def main():
     filename = "rosters.txt"
     ivy_schools = ["Harvard", "Princeton", "Yale", "Brown", "UPenn", "Columbia", "Cornell", "Dartmouth"]
     print("WELCOME TO THE IVY LEAGUE DUAL MEET SIMULATOR! \nYour options are: Harvard, Princeton, Yale, Brown, UPenn, Columbia, Cornell, Dartmouth\n")
+
+    # Makes user choose 2 schools, prints error message if user types something incorrect
     while True:
         school1 = input("Choose School #1: ").strip()
         if school1 not in ivy_schools:
@@ -48,9 +56,12 @@ def main():
             print("Sorry, but this is an Ivy League dual meet simulator. Please choose an Ivy League school.")
             continue
         break
+
+
     all_teams = get_teams(filename)
     teams = {school1: all_teams[school1], school2: all_teams[school2],}
 
+    # Creates Meet object to simulate the meet
     meet = Meet(teams, EVENTS)
     meet.assign_events()
     meet.simulate_meet()
