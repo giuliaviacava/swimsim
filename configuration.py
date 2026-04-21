d2a84582c232baec639526d433f55831db4744de
davinahuang07 <davinahuang07@gmail.com>
2026-04-21 03:08:10 +0000

    Mon, Apr 20, 2026, 11:08 PM -04:00

--- configuration.py
+++ configuration.py
@@ -1,6 +1,6 @@
 ### swimsim/configuration.py
 '''
-Defines what events exist, how scoring works, participation limits, random add/drop time variations
+Defines what events exist, how scoring works, random add/drop time variations
 '''
 
 EVENTS = ["50 Free", "100 Free", "200 Free", "500 Free", "1000 Free", "100 Back", "200 Back", "100 Breast", "200 Breast", "100 Fly", "200 Fly", "200 IM", "400 IM",]
