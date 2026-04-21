5a2a9b4d463ca405f970e4dcde197fc4c14b5e5e
davinahuang07 <davinahuang07@gmail.com>
2026-04-21 03:12:01 +0000

    Mon, Apr 20, 2026, 11:12 PM -04:00

--- meet.py
+++ meet.py
@@ -5,3 +5,10 @@ simulate all events
 convert times from seconds, milliseconds back to minutes, seconds, milliseconds
 print all results
 '''
+from event import Event
+
+class Meet:
+    def __init__(self, teams, event_names):
+        self.teams = teams
+        self.event_names = event_names
+        self.events = [Event(name) for name in event_names]
