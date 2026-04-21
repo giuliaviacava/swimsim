242328801f550791b211f74bd14ec7ef70f4a389
davinahuang07 <davinahuang07@gmail.com>
2026-04-21 02:58:36 +0000

    Mon, Apr 20, 2026, 10:58 PM -04:00

--- configuration.py
+++ configuration.py
@@ -6,7 +6,5 @@ Defines what events exist, how scoring works, participation limits, random add/d
 EVENTS = ["50 Free", "100 Free", "200 Free", "500 Free", "1000 Free", "100 Back", "200 Back", "100 Breast", "200 Breast", "100 Fly", "200 Fly", "200 IM", "400 IM",]
 SCORING = [9, 4, 3, 2, 1]
 
-MAX_EVENTS_PER_SWIMMER = 3
-
 MIN_VARIATION = -0.02
 MAX_VARIATION = 0.05
