b516f47e75dd46d8d59bf9e2b59ae95d569cae3f
davinahuang07 <davinahuang07@gmail.com>
2026-04-23 15:35:28 +0000

    Thu, Apr 23, 2026, 11:35 AM -04:00

--- configuration.py
+++ configuration.py
@@ -1,10 +1,7 @@
 ### swimsim/configuration.py
 '''
-Defines what events exist, how scoring works, random add/drop time variations
+Defines what events exist, how scoring works
 '''
 
 EVENTS = ["50 Free", "100 Free", "200 Free", "500 Free", "1000 Free", "100 Back", "200 Back", "100 Breast", "200 Breast", "100 Fly", "200 Fly", "200 IM", "400 IM",]
 SCORING = [9, 4, 3, 2, 1]
-
-MIN_VARIATION = -0.02
-MAX_VARIATION = 0.05
--- simulator.py
+++ simulator.py
@@ -1,3 +1,25 @@
-'''
-Two methods, one to generate swimmer times and one to simulate single event
-'''
+### swimsim/simulator.py
+
+import random
+from configuration import SCORING
+
+# Random add/drop variations, swimmers can drop up to 2% or add up to 5%
+MIN_VARIATION = -0.02
+MAX_VARIATION = 0.05
+
+# Generates swimmer's time by using their best time and adding the random variation
+def generate_swimmer_time(swimmer, event_name):
+    best_time = swimmer.best_time_for(event_name)
+    return best_time * (1 + random.uniform(MIN_VARIATION, MAX_VARIATION))
+
+# Generates a time for each swimmer in the event and adds that time to a list of results
+def simulate_event(event, teams):
+    results = []
+    for swimmer in event.entries:
+        simulated_time = generate_swimmer_time(swimmer, event.name)
+        results.append((swimmer, simulated_time))
+    results.sort(key=lambda x: x[1])
+    event.set_results(results)
+    for place, (swimmer, _) in enumerate(results[:len(SCORING)]):
+        points = SCORING[place]
+        teams[swimmer.team].add_points(points)
