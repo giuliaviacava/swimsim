fc37a1575d7912a73ca914056def39affa4f3101
davinahuang07 <davinahuang07@gmail.com>
2026-04-27 20:23:26 +0000

    Mon, Apr 27, 2026, 4:23 PM -04:00

--- meet.py
+++ meet.py
@@ -21,14 +21,10 @@ class Meet:
         self.event_names = event_names
         self.events = [Event(name) for name in event_names]
 
-    # Assigns swimmers from each team to events (use method from lineupbuilder)
+    # Assigns swimmers from each team to events (use method from lineupbuilder), prints assignments
     def assign_events(self):
         assign_all_teams(self.teams, self.event_names)
-        self.check_assignments()
-
-    #
-    def check_assignments(self):
-        print("\n===== EVENT ASSIGNMENTS =====")
+        print("\n----- EVENT ASSIGNMENTS -----")
         for team in self.teams.values():
             print(f"\n{team.name}")
             for swimmer in team.swimmers:
@@ -36,7 +32,6 @@ class Meet:
 
     def build_event_entries(self):
         for event in self.events:
-
             for team in self.teams.values():
                 for swimmer in team.swimmers:
                     if event.name in swimmer.assigned_events:
@@ -50,7 +45,7 @@ class Meet:
             self.print_event_results(event)
 
     def print_event_results(self, event):
-        print(f"\n===== {event.name} =====")
+        print(f"\n----- {event.name} -----")
 
         if not event.results:
             print("No swimmers entered.")
@@ -66,7 +61,7 @@ class Meet:
                 print(f"{place}. {swimmer.name} ({swimmer.team}) - {formatted_time}")
 
     def print_final_results(self):
-        print("\n===== FINAL TEAM SCORES =====")
+        print("\n----- FINAL TEAM SCORES -----")
 
         ranked_teams = sorted(
             self.teams.values(),
